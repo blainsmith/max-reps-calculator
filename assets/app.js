ae6d08c999aca456309c23b3f32458f30007bc10
Blain Smith <rebelgeek@blainsmith.com>
2014-07-13 11:36:44 -0400

    new formulas and colors

--- assets/app.js
+++ assets/app.js
@@ -1,28 +1,81 @@
+$(function() {
+  $('nav a').click(function() {
+    var $el = $(this);
+
+    if(!$el.hasClass('active')) {
+      var view = $el.attr('data-view');
+      
+      $('.maxes').hide();
+      $('.' + view).show();
+      
+      $('nav a').removeClass('active');
+      $el.addClass('active');
+    }
+  });
+});
+
 function AppViewModel(){
   var self = this;
 
-  self.coeffs = [1, .95, .93, .90, .87, .85, .83, .80, .77, .75];
+  // Initialize with some defaults
   self.defaultWeight = 200;
   self.defaultReps = 3;
 
+  // Set the observable inputs
   self.weight = ko.observable(self.defaultWeight);
   self.reps = ko.observable(self.defaultReps);
-  self.maxes = ko.observableArray();
+  
+  // Set the max arrays
+  self.maxes = {
+    lombardi: ko.observableArray(),
+    bryzcki: ko.observableArray(),
+    epley: ko.observableArray(),
+    average: ko.observableArray()
+  };
+  
+  // Compute all the things
   self.computeMaxes = ko.computed(function(){
+
+    // Error checking
     if(isNaN(self.weight()) || self.weight() == '') { self.weight(self.defaultWeight); }
     if(isNaN(self.reps()) || self.reps() == '') { self.reps(self.defaultReps); }
     if(self.reps() > 10) { self.reps(10); }
 
+    // Positive whole numbers only
     self.weight(Math.abs(self.weight()));
     self.reps(Math.round(Math.abs(self.reps())));
 
-    var single = Math.round(self.weight() / self.coeffs[self.reps() - 1]);
+    // Ensure the arrays are clear
+    self.maxes.lombardi.removeAll();
+    self.maxes.bryzcki.removeAll();
+    self.maxes.epley.removeAll();
+    self.maxes.average.removeAll();
 
-    self.maxes.removeAll();
+    // Compute the 1RM of each formula
+    self.maxes.lombardi.push(Math.floor(self.weight() * Math.pow(self.reps(), .10)));
+    self.maxes.bryzcki.push(Math.floor(self.weight() * (36 / (37 - self.reps()))));
+    self.maxes.epley.push(Math.floor(self.weight() * (1 + (self.reps() / 30))));
+    
+    // Compute the average 1RM
+    self.maxes.average.push(Math.floor(
+      (self.maxes.lombardi()[0] +
+      self.maxes.bryzcki()[0] +
+      self.maxes.epley()[0]) / 3
+    ));
 
-    ko.utils.arrayForEach(self.coeffs, function(coeff){
-      self.maxes.push(Math.round(single * coeff));
-    });
+    for(reps = 2; reps <= 10; reps++) {
+      // Compute the 2-10RM of each formula
+      self.maxes.lombardi.push(Math.floor(self.maxes.lombardi()[0] / Math.pow(reps, .10)));
+      self.maxes.bryzcki.push(Math.floor(self.maxes.bryzcki()[0] * (37 - reps) / 36));
+      self.maxes.epley.push(Math.floor(self.maxes.epley()[0] / ((1 + (reps / 30)))));
+
+      // Compute the average 2-10RM
+      self.maxes.average.push(Math.floor(
+        (self.maxes.lombardi()[reps - 1] +
+        self.maxes.bryzcki()[reps - 1] +
+        self.maxes.epley()[reps - 1]) / 3
+      ));
+    }
   });
 }
 
